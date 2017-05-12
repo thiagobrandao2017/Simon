ec4bb7d35de020f7f7982c5267bf0a05740a545a
Thiago <thiagoqdrb@gmail.com>
2017-05-12 10:41:46 -0400

    complete logic

--- app.js
+++ app.js
@@ -1,34 +1,51 @@
 $(document).ready(function(){
 
-  var green = $("#green");
-  var red = $("#red");
-  var blue = $("#blue");
-  var yellow = $("#yellow");
-  var colors = [green,red,blue,yellow];
-  var random = colors[Math.floor(Math.random() * 4)].attr("id");
+  var colors = ["green","red","blue","yellow"];
   var compSequence = [];
   var userSequence = [];
 
+  var start = function() {
+    userSequence = [];
+    var random = colors[Math.floor(Math.random() * 4)];
+    compSequence.push(random);
 
-  $("#start").on("click",function(){
-     $(`#${random}`).addClass("click-bright-" + random);
-     setTimeout(function() {
-       $(`#${random}`).removeClass("click-bright-" + random);
-     }, 2000);
-     compSequence.push(random);
- });
+    console.log(compSequence);
 
- $(".color-selection").on("click", function() {
+    for (var i = 0; i < compSequence.length; i++) {
+      $(`#${random}`).addClass("click-bright-" + random);
+        setTimeout(function() {
+          $(`#${random}`).removeClass("click-bright-" + random);
+        },2000)
+    }
+  }
+
+  var compare = function() {
+    for (var i = 0; i < compSequence.length; i++) {
+      if (compSequence[i] !== userSequence[i]) {
+        alert("Wrong, start again!");
+        return;
+      }
+    }
+    start();
+  }
+
+  var userClick = function() {
     var colorSelected = $(this).attr("id");
-    $(this)
-    .addClass("click-bright-" + colorSelected)
-    var that = this;
+    $(this) .addClass("click-bright-" + colorSelected);
+    var that = $(this);
     setTimeout(function() {
-      $(that).removeClass("click-bright-" + colorSelected);
+      that.removeClass("click-bright-" + colorSelected);
     }, 2000);
     userSequence.push(colorSelected);
-  });
+    if (userSequence.length === compSequence.length) {
+      setTimeout(function() {
+        compare();
+      }, 2000);
+    }
+    console.log(userSequence);;
+  }
 
+  $("#start").on("click", start);
+  $(".color-selection").on("click", userClick);
 
-
-})
+});
